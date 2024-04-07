1d4caaa6162b92a47b96a5b81749a97b9dc9ccd5
MattWong-ca <matthew.yt.wong@gmail.com>
2024-04-07 12:03:39 -0400

    Add template code

---
+++ clone/api/index.tsx
@@ -0,0 +1,100 @@
+import { Button, Frog } from "frog";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
+import { neynar as neynarHub } from "frog/hubs";
+import { neynar } from "frog/middlewares";
+import { handle } from "frog/vercel";
+import { CastParamType, NeynarAPIClient } from "@neynar/nodejs-sdk";
+import { Box, Heading, VStack, vars } from "../lib/ui.js";
+
+const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY ?? "";
+const neynarClient = new NeynarAPIClient(process.env.NEYNAR_API_KEY!);
+
+const ADD_URL =
+  "https://warpcast.com/~/add-cast-action?name=Upthumb&icon=thumbsup&actionType=post&postUrl=https://upthumbs.app/api/upthumb";
+
+export const app = new Frog({
+  assetsPath: "/",
+  basePath: "/api",
+  ui: { vars },
+  hub: neynarHub({ apiKey: NEYNAR_API_KEY }),
+  browserLocation: ADD_URL,
+}).use(
+  neynar({
+    apiKey: NEYNAR_API_KEY,
+    features: ["interactor", "cast"],
+  })
+);
+
+// Cast action handler
+app.hono.post("/upthumb", async (c) => {
+  const {
+    trustedData: { messageBytes },
+  } = await c.req.json();
+
+  const result = await neynarClient.validateFrameAction(messageBytes);
+  if (result.valid) {
+    const cast = await neynarClient.lookUpCastByHashOrWarpcastUrl(
+      result.action.cast.hash,
+      CastParamType.Hash
+    );
+    const {
+      cast: {
+        author: { fid, username },
+      },
+    } = cast;
+    if (result.action.interactor.fid === fid) {
+      return c.json({ message: "Nice try." }, 400);
+    }
+
+    console.log("FID: ", fid);
+    console.log("Username: ", username);
+
+    let message = `You upthumbed ${username}`;
+    if (message.length > 30) {
+      message = "Upthumbed!";
+    }
+
+    return c.json({ message });
+  } else {
+    return c.json({ message: "Unauthorized" }, 401);
+  }
+});
+
+// Frame handlers
+app.frame("/", (c) => {
+  return c.res({
+    image: (
+      <Box
+        grow
+        alignVertical="center"
+        backgroundColor="white"
+        padding="32"
+        border="1em solid rgb(138, 99, 210)"
+      >
+        <VStack gap="4">
+          <Heading color="fcPurple" align="center" size="64">
+            Upthumbs 👍
+          </Heading>
+        </VStack>
+      </Box>
+    ),
+    intents: [
+      <Button.Link href={ADD_URL}>Add Action</Button.Link>,
+      <Button value="leaderboard" action="/leaderboard">
+        🏆 Leaderboard
+      </Button>,
+      <Button value="start" action="/upthumbs">
+        👍 My Upthumbs
+      </Button>,
+    ],
+  });
+});
+
+// @ts-ignore
+const isEdgeFunction = typeof EdgeFunction !== "undefined";
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
+devtools(app, isProduction ? { assetsPath: "/.frog" } : { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
