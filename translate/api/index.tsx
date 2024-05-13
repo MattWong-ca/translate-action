9f3b5b948089e546b61ec9fbbf0fc176af66154e
MattWong-ca <matthew.yt.wong@gmail.com>
2024-05-13 09:03:37 -0400

    Complete update to multi step action

--- translate/api/index.tsx
+++ translate/api/index.tsx
@@ -4,22 +4,16 @@ import { serveStatic } from "frog/serve-static";
 import { neynar as neynarHub } from "frog/hubs";
 import { neynar } from "frog/middlewares";
 import { handle } from "frog/vercel";
-import { CastParamType, NeynarAPIClient } from "@neynar/nodejs-sdk";
 import { vars } from "../lib/ui.js";
 import OpenAI from "openai";
 
 const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY!;
-const neynarClient = new NeynarAPIClient(NEYNAR_API_KEY);
-
-const ADD_URL =
-  "https://warpcast.com/~/add-cast-action?name=Translate+to+English&icon=comment&actionType=post&postUrl=https://translate-action.vercel.app/api/english";
 
 export const app = new Frog({
   assetsPath: "/",
   basePath: "/api",
   ui: { vars },
   hub: neynarHub({ apiKey: NEYNAR_API_KEY }),
-  browserLocation: ADD_URL,
 }).use(
   neynar({
     apiKey: NEYNAR_API_KEY,
@@ -85,66 +79,6 @@ app.frame('/view', async (c) => {
     ],
   })
 })
-
-// Cast action handler
-// app.hono.post("/english", async (c) => {
-//   const {
-//     trustedData: { messageBytes },
-//   } = await c.req.json();
-
-//   const result = await neynarClient.validateFrameAction(messageBytes);
-//   if (result.valid) {
-//     const cast = await neynarClient.lookUpCastByHashOrWarpcastUrl(
-//       result.action.cast.hash,
-//       CastParamType.Hash
-//     );
-//     const {
-//       cast: {
-//         // author: { fid, username },
-//         text,
-//         hash
-//       },
-//     } = cast;
-
-//     const completion = await openai.chat.completions.create({
-//       messages: [
-//         { role: "system", content: 'You are a translation bot that translates text to English. If the text is already in English, please respond appropriately.' },
-//         { role: "user", content: `Translate this to English: ${text}` }
-//       ],
-//       model: "gpt-3.5-turbo",
-//     });
-
-//     let openaiResponse = completion.choices[0].message.content;
-//     if (openaiResponse && openaiResponse.length > 320) {
-//       const replyOne = await neynarClient.publishCast(
-//         process.env.SIGNER_UUID!,
-//         openaiResponse.slice(0, 320),
-//         {
-//           replyTo: hash,
-//         }
-//       );
-//       await neynarClient.publishCast(
-//         process.env.SIGNER_UUID!,
-//         openaiResponse.slice(320),
-//         {
-//           replyTo: replyOne.hash,
-//         }
-//       );
-//     } else {
-//       await neynarClient.publishCast(
-//         process.env.SIGNER_UUID!,
-//         openaiResponse!,
-//         {
-//           replyTo: hash,
-//         }
-//       );
-//     }
-
-//     return c.json({ message: "Translated! ✅" });
-//   } else {
-//     return c.json({ message: "Error, please try again!" }, 401);
-//   }
-// });
 
 // @ts-ignore
 const isEdgeFunction = typeof EdgeFunction !== "undefined";
